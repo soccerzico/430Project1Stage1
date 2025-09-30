a950fa0923059d205170ba16fc48d0ccee3ad430
Zaki Adwan <soccerzico@gmail.com>
2025-09-30 02:56:38 +0000

    Final changes

--- Stage1Implementation/Inventory.java
+++ Stage1Implementation/Inventory.java
@@ -2,47 +2,62 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+
+import java.util.*;
+
 public class Inventory {
-  private List products = new LinkedList();
+  private final List<Product> products = new LinkedList<>();
   private static Inventory inventory;
 
-  private Inventory() {
-  }
+  private Inventory() {}
+
   public static Inventory instance() {
     if (inventory == null) {
-      return (inventory = new Inventory());
-    } else {
-      return inventory;
+      inventory = new Inventory();
     }
-  }
-  
-  //Add and remove products from inventory's collection
-  public boolean AddProduct(Product product) {
-    products.add(product);
-    return true;
+    return inventory;
   }
 
-  public boolean RemoveProduct(Product product) {
-    products.remove(product);
-    return true;
+  public boolean addProduct(Product product) {
+    return products.add(product);
   }
 
-  //Use to traverse the inventory's collection.
-  public Iterator GetProducts() {
+  public boolean removeProduct(Product product) {
+    return products.remove(product);
+  }
+
+  public Iterator<Product> getProducts() {
     return products.iterator();
   }
- 
+
+  @Override
   public String toString() {
     return products.toString();
   }
-  
-  //Check if product is in inventory.
-  public boolean CheckInventory(Iterator GetProducts, Product product){
-    while (products.hasNext()){
-        if (products.next() == product){
-            return true;
-        }
-     }
+  public void displayInventory() {
+    System.out.println("=== Inventory ===");
+    int totalQuantity = 0;
+
+    for (Product p : products) {
+        System.out.println(p); // relies on Product.toString()
+        totalQuantity += p.getQuantity();
+    }
+
+    System.out.println("----------------");
+    System.out.println("Total distinct products: " + products.size());
+    System.out.println("Total items in stock: " + totalQuantity);
+}
+
+
+  public boolean checkInventory(Iterator<Product> iter, Product target) {
+    if (iter == null || target == null) return false;
+    while (iter.hasNext()) {
+      Product p = iter.next();
+      if (target.equals(p)) {       // relies on Product.equals
+        return true;
+      }
+    }
     return false;
   }
 }
+
